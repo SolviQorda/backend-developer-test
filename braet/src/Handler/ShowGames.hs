6fd610907889746db1b072c6455b8fcf23654b86
SolviQorda <solvi.goard@gmail.com>
2019-01-16 10:59:35 +0000

    Show games GET refactored for rawSql / postGIS query for position

--- braet/src/Handler/ShowGames.hs
+++ braet/src/Handler/ShowGames.hs
@@ -12,26 +12,27 @@ import Import
 import qualified Data.Text as T
 import Database.Persist.Sql
 
+-- getShowGamesR :: Handler Value
+-- getShowGamesR = do
+--   (authId, user) <- requireAuthPair
+--   games <- runDB $ selectList [HostedGameHostId ==. authId] [Asc HostedGameTitle]
+--   return $ toJSON $ Prelude.map (\r -> entityVal r) games
+
 getShowGamesR :: Handler Value
 getShowGamesR = do
-  (authId, user) <- requireAuthPair
-  games <- runDB $ selectList [HostedGameHostId ==. authId] [Asc HostedGameTitle]
-  return $ toJSON $ Prelude.map (\r -> entityVal r) games
-
--- getShowGamesR :: UserId -> Handler Value
--- getShowGamesR userId = do
---   -- TODO: switch this with selectFirst for safety.
---   user  <- runDB $ selectList [UserProfilePlayerId ==. (pack $ show userId)] [Asc UserProfilePlayerId]
---   games <- selectGames (userGames user) (fst $ userLongLat user) (snd $ userLongLat user)
---   return $ toJSON $ Prelude.map entityVal $ games
---     where
---       selectGames
---         :: [Text]
---         -> Double
---         -> Double
---         -> Handler [Entity HostedGame]
---       selectGames t u v = runDB $ rawSql s [toPersistValue t, toPersistValue u, toPersistValue v]
---         where s = "SELECT ?? FROM hosted_game WHERE title IN ? ORDER BY ST_DISTANCESPHERE(ST_POINT(?,?), ST_POINT(latitude,longitude))"
+  -- TODO: switch this with selectFirst for safety.
+    (authId, user) <- requireAuthPair
+    user  <- runDB $ selectList [UserProfilePlayerId ==. (authId)] [Asc UserProfilePlayerId]
+    games <- selectGames (userGames user) (fst $ userLongLat user) (snd $ userLongLat user)
+    return $ toJSON $ Prelude.map entityVal $ games
+      where
+        selectGames
+          :: [Text]
+          -> Double
+          -> Double
+          -> Handler [Entity HostedGame]
+        selectGames t u v = runDB $ rawSql s [toPersistValue t, toPersistValue u, toPersistValue v]
+          where s = "SELECT ?? FROM hosted_game WHERE title IN ? ORDER BY ST_DISTANCESPHERE(ST_POINT(?,?), ST_POINT(latitude,longitude))"
 
 userLongLat :: [Entity UserProfile] -> (Double, Double)
 userLongLat ps =
